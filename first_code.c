85dcf145759bf942b5ca7af7fd651593ae64ac49
Siddurn <157235129+Siddurn@users.noreply.github.com>
2024-07-23 17:24:27 +0530

    Create first_code.c
    This code will help to know the basic structure of c program

---
+++ first_code.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // Preprocessor directive for standard input-output library
+
+int main() {
+    // This is the main function where the program execution begins
+
+    printf("Hello, World!\n"); // Prints "Hello, World!" to the console
+    return 0; // Returns 0 to indicate successful execution
+}
